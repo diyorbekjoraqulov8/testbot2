5d340b8fa3ba7fb76011e6cdd3f1839d96d49f1a
diyorbekjoraqulov8 <diyorbekjoraqulov8@gmail.com>
2023-11-21 17:05:27 +0300

    Two 2 version simple bot

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const TelegramBot = require('node-telegram-bot-api');
-const TOKEN = "6682255149:AAFej1fhjbho4g2QE_f8jJzAQYnBvpM7hpM";
+const TOKEN = "6682255149:AAF8hwYf3ffwJ999DcM7FgTVGrdtlLPXB6I";
+const port = 3000;
 
 const bot = new TelegramBot(TOKEN, { polling: true });
 
@@ -15,3 +16,19 @@ bot.on('message', (msg) => {
     bot.sendMessage(chatId, "Comand not found");
   }
 });
+
+app.use(bodyParser.json());
+
+// Define the endpoint for receiving updates
+app.post('/https://telegrambot2-ayvt.onrender.com', (req, res) => {
+  bot.processUpdate(req.body);
+  res.sendStatus(200);
+});
+
+// Start the Express server
+app.listen(port, () => {
+  console.log(`Express server is listening on port ${port}`);
+});
+
+// Set the webhook for the bot
+bot.setWebHook('https://your-server.com/your-webhook-path');
